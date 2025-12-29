454600337fd5e59e8152a32baa65319e20e0c71c
Aparajita Tiwari <aparajitatiwari202@gmail.com>
2025-12-30 00:20:52 +0530

    Update all files

--- src/index.js
+++ src/index.js
@@ -3,33 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = mysql.createPool({
-  host: process.env.localhost,
-  user: process.env.root,
-  password: process.env.Aparajita@10,
-  database: process.env.merit_portal,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
-
-db.getConnection((err, connection) => {
+connection.connect((err) => {
   if (err) {
     console.error("❌ MySQL connection failed:", err.message);
-  } else {
-    console.log("✅ MySQL connected successfully");
-    connection.release();
+    return;
   }
-});
-
-export default db;
-
-
-import app from "./app.js";
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(" MySQL connected successfully");
 });
